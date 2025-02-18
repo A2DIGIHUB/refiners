291eb2776ec332782182c58e5623890ef973d66d
Generoso Lopez <generosolopez@gmail.com>
2025-02-18 11:56:25 +0100

    Added Vercel adapter and configured SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import compress from 'astro-compress';
+import vercel from '@astrojs/vercel/adapter';
 
 export default defineConfig({
   integrations: [
     tailwind(),
     react(),
-    compress(),
   ],
+  output: 'server',
+  adapter: vercel(),
   build: {
     inlineStylesheets: 'auto',
     splitting: true,
