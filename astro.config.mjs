2cae5f7a155cac7e5c5e84127191a1b24fd3a8da
Generoso Lopez <generosolopez@gmail.com>
2025-02-18 11:59:28 +0100

    Fixed Vercel adapter import for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import compress from 'astro-compress';
-import vercel from '@astrojs/vercel/adapter';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [
